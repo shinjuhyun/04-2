e4dfd002beb1115b18db64fd5df347f922f2d209
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-09-27 16:52:27 +0900

    01

--- main.c
+++ main.c
@@ -4,11 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-
-	printf("input the second : ");
-	scanf("%i",&x);
+	int x,y,z,m;
+	int a,b,c;
+	x=1;
+	y=3;
+	z=5;
+	a=3;
+	b=4;
+	c=5;
 	
-	printf("%i second is %i : %i",x, x/60,x%60);
+	y=a*x*x+b*x+c;
+	m=(x+y+z)/3;
+	
+	printf("y=%d,m=%d",y,m);
+
 	return 0;
 }
